99b7a78443fc1ede02d254af3a0613a6e7fa5774
emilwhatsapp <117849900+emilwhatsapp@users.noreply.github.com>
2024-05-03 23:20:10 +0700

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let พร็อกซีไอพี = พร็อกซีไอพีs[Math.
 // let พร็อกซีไอพี = 'cdn.xn--b6gac.eu.org';
 // ipv6 พร็อกซีไอพี example remove comment to use
 // let พร็อกซีไอพี = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
-let URLDIRECT = 't.me/trust_bodong';
+let URLDIRECT = 'google.com';
 let dohURL = 'https://dns.adguard.com/dns-query';
 
 if (!isValidUUID(userID)) {
@@ -915,4 +915,4 @@ function สร้างวเลสSub(ไอดีผู้ใช้_เส้
 	return ผลลัพธ์.join('\n');
 }
 
-const cn_hostnames = [linkDirect];
+const cn_hostnames = [URLDIRECT];
