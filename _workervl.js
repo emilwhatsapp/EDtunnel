b2890f7abe8c4100f13e40b064724c059fc16cd0
emilwhatsapp <117849900+emilwhatsapp@users.noreply.github.com>
2024-06-04 22:04:34 +0700

    Add files via upload

--- _workervl.js
+++ _workervl.js
@@ -753,8 +753,8 @@ Gak Ada apa apa disini :).
 `
 } else {
 return `
-<h1 id="header">SILAHKAN PILIH</h1>
-<div id="cfgvid" style="display: none;">
+<h1>SILAHKAN PILIH SETTINGAN INJECT ANDA</h1>
+<div id="cfgvid" class="divContent" style="display: none;">
       <h2 style="text-align: center;">VLESS CLOUDFLARE FREE</h2>
   <em><span style="color: red;">NOTE:</span> Settingan ini untuk inject paket VIDIO, kalian tinggal salin dan tempel pada apk yang kalian gunakan untuk inject.</em>
   ===========================================
@@ -917,7 +917,8 @@ function showText(divId) {
     resetButton.classList.add('button'); // Menambahkan kelas button
     resetButton.onclick = function() {
         // Mengatur ulang halaman ke kondisi awal
-            document.querySelectorAll('.button').forEach(button => {
+        document.getElementById('header').innerText = 'SILAHKAN PILIH';
+        document.querySelectorAll('.button').forEach(button => {
             button.style.display = 'inline-block';
         });
         resultDiv.innerHTML = ''; // Menghapus isi dari resultDiv
